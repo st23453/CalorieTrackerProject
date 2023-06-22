68c289adc9c2ebcb34bf0564e4984575a3c0b463
Nathan Dalpatan <st23453@ormiston.school.nz>
2023-06-22 23:40:47 +1200

    Update loginpage.py

--- loginpage.py
+++ loginpage.py
@@ -18,13 +18,13 @@ def login_function():
     label.place(relx=0.5, rely=0.5,  anchor=tk.CENTER)
     w.mainloop()
 
-user_entry = customtkinter.CTkEntry(master=app, width=220, placeholder_text='Username or Email', font=('Switzer',16, "bold"))
-user_entry.place(relx=0.5, rely=0.45, anchor=tk.CENTER)
+user_entry = customtkinter.CTkEntry(master=app, width=220, placeholder_text='Username or Email', font=('Switzer',16))
+user_entry.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
 
 password_entry = customtkinter.CTkEntry(master=app, width=220, placeholder_text='Password', show="⚫", font= ("Switzer", 16))
-password_entry.place(relx=0.5, rely=0.55, anchor=tk.CENTER)
+password_entry.place(relx=0.5, rely=0.45, anchor=tk.CENTER)
 
 login_button = customtkinter.CTkButton(master=app, width=220, text="Login", command=login_function, corner_radius=6)
-login_button.place(relx=0.5, rely=0.65, anchor=tk.CENTER)
+login_button.place(relx=0.5, rely=0.55, anchor=tk.CENTER)
 
 app.mainloop()
