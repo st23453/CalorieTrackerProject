7121cc180d8b36f71960461525d72548caa00149
Nathan Dalpatan <st23453@ormiston.school.nz>
2023-06-22 23:33:00 +1200

    minor changes
    changed variable names and added font style and size

--- loginpage.py
+++ loginpage.py
@@ -13,15 +13,15 @@ def login_function():
     app.destroy()            # destroy current window and creating new one 
     w = customtkinter.CTk()  
     w.geometry("1280x720")
-    w.title('Welcome')
-    l1=customtkinter.CTkLabel(master=w, text="Home Page",font=('Century Gothic',60))
-    l1.place(relx=0.5, rely=0.5,  anchor=tk.CENTER)
+    w.title('Homepage')
+    label=customtkinter.CTkLabel(master=w, text="Home Page",font=('Century Gothic',60))
+    label.place(relx=0.5, rely=0.5,  anchor=tk.CENTER)
     w.mainloop()
 
-user_entry = customtkinter.CTkEntry(master=app, width=220, placeholder_text='Username or Email')
+user_entry = customtkinter.CTkEntry(master=app, width=220, placeholder_text='Username or Email', font=('Switzer',16, "bold"))
 user_entry.place(relx=0.5, rely=0.45, anchor=tk.CENTER)
 
-password_entry = customtkinter.CTkEntry(master=app, width=220, placeholder_text='Password', show="⚫")
+password_entry = customtkinter.CTkEntry(master=app, width=220, placeholder_text='Password', show="⚫", font= ("Switzer", 16))
 password_entry.place(relx=0.5, rely=0.55, anchor=tk.CENTER)
 
 login_button = customtkinter.CTkButton(master=app, width=220, text="Login", command=login_function, corner_radius=6)
