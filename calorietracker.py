04a7691b4d4f21ee7e8579ae0fce0132241e8d2a
Nathan Dalpatan <st23453@ormiston.school.nz>
2023-07-17 10:10:03 +1200

    frames for homepage
    positioned frames

--- calorietracker.py
+++ calorietracker.py
@@ -20,31 +20,36 @@ def homescreen_function():
     loginpage.destroy()  # Destroy current window and create a new one
     
     homepage = ctk.CTk()  # Creating homepage window
+    homepage.geometry("1280x750")
+    homepage.title('Homepage')
+    homepage = ctk.CTk()  # Creating homepage window
+   
     homepage.geometry("850x500")
     homepage.title('Homepage')
     homepage.maxsize(900, 600)
     homepage.configure(fg_color="#232635")
 
     #Homepage Frame
-    
-    menu1_frame = ctk.CTkFrame(master=homepage, width=200, height=800, border_width=4)
-    menu1_frame.pack(side="right")
 
-    entry_frame = ctk.CTkFrame(master=homepage, width=600, height=200, border_width=4)
-    entry_frame.pack(side="left")
+    #main frames  
+    menu1_frame = ctk.CTkFrame(master=homepage, width=200, height=800, fg_color="transparent")
+    menu1_frame.pack(side = "right", fill = "both", expand = True) #right of the page
 
-    info_frame = ctk.CTkFrame(master=homepage, width=600, height=200, border_width=4)
-    info_frame.pack(side="left", pady = 150)  # Moved info_frame below entry_frame
+    menu2_frame = ctk.CTkFrame(master=homepage, width=200, height=800,fg_color="transparent")
+    menu2_frame.pack(side = "left", fill = "both", expand = True) #right of the page
 
-    # Homescreen widgets in entry_frame
-    label_entry_frame = ctk.CTkLabel(master=entry_frame, text="Widgets inside entry_frame")
-    label_entry_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    #frames inside the main frame 
 
-    # Homescreen widgets in info_frame
-    label_info_frame = ctk.CTkLabel(master=info_frame, text="Widgets inside info_frame")
-    label_info_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-    # Homescreen widgets
-    
+    user_frame = ctk.CTkFrame(master=menu1_frame, width=200, height=800, corner_radius=20)
+    user_frame.pack(padx = "10", pady = "20") #top of the page
+
+    entry_frame = ctk.CTkFrame(master=menu2_frame, width=600, height=250, corner_radius=20)
+    entry_frame.pack(side= "top", padx = 10, pady = 20) #top of the page
+
+    info_frame = ctk.CTkFrame(master=menu2_frame, width=600, height=200, corner_radius=20)
+    info_frame.pack(side = "bottom", padx = 10, pady = 20)  #bottom of the page
+
+    #buttons inside entry_frame
 
     homepage.mainloop()
 
--- homepage.py
+++ homepage.py
@@ -1,14 +1,39 @@
+import tkinter as tk
 import customtkinter as ctk
+
 
 homepage = ctk.CTk()  # Creating homepage window
 homepage.geometry("1280x750")
 homepage.title('Homepage')
+homepage = ctk.CTk()  # Creating homepage window
+   
+homepage.geometry("850x500")
+homepage.title('Homepage')
+homepage.maxsize(900, 600)
+homepage.configure(fg_color="#232635")
 
-    #Homepage Frame
-frame = ctk.CTkFrame(master=homepage, width=200, height=200, border_width=4)
-frame.pack()
+#Homepage Frame
 
-    # Homescreen widgets
-    
+#main frames  
+menu1_frame = ctk.CTkFrame(master=homepage, width=200, height=800, fg_color="transparent")
+menu1_frame.pack(side = "right", fill = "both", expand = True) #right of the page
+
+menu2_frame = ctk.CTkFrame(master=homepage, width=200, height=800,fg_color="transparent")
+menu2_frame.pack(side = "left", fill = "both", expand = True) #right of the page
+
+#frames inside the main frame 
+
+user_frame = ctk.CTkFrame(master=menu1_frame, width=200, height=800, corner_radius=20)
+user_frame.pack(padx = "10", pady = "20") #top of the page
+
+entry_frame = ctk.CTkFrame(master=menu2_frame, width=600, height=250, corner_radius=20)
+entry_frame.pack(side= "top", padx = 10, pady = 20) #top of the page
+
+info_frame = ctk.CTkFrame(master=menu2_frame, width=600, height=200, corner_radius=20)
+info_frame.pack(side = "bottom", padx = 10, pady = 20)  #bottom of the page
+
+#buttons inside entry_frame
+
+
 
 homepage.mainloop()
